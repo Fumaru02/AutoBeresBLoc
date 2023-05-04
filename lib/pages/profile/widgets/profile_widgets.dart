15f3c87af748187ca1685f04aa55614a438d77f1
Fumaru02 <120907247+Fumaru02@users.noreply.github.com>
2023-05-05 05:46:41 +0700

    update profile with BLoc

--- lib/pages/profile/widgets/profile_widgets.dart
+++ lib/pages/profile/widgets/profile_widgets.dart
@@ -6,6 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 AppBar buildAppbar() {
   return AppBar(
+    backgroundColor: Colors.black.withOpacity(0.9),
     title: Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -15,11 +16,14 @@ AppBar buildAppbar() {
             height: 12.h,
             child: Image.asset("assets/icons/menu.png"),
           ),
-          reusableTextGlobal("Profile"),
+          reusableTextGlobal("Profile", color: Colors.white),
           SizedBox(
             width: 24.w,
             height: 24.h,
-            child: Image.asset("assets/icons/more-vertical.png"),
+            child: Image.asset(
+              "assets/icons/more-vertical.png",
+              color: Colors.white,
+            ),
           ),
         ],
       ),
@@ -30,14 +34,15 @@ AppBar buildAppbar() {
 //profile icon and edit button
 Widget profileIconAndEditButton() {
   return Container(
+    margin: EdgeInsets.only(top: 20.h),
     alignment: Alignment.bottomRight,
     padding: EdgeInsets.only(right: 6.w),
-    width: 80.w,
-    height: 80.h,
+    width: 100.w,
+    height: 100.h,
     decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20.w),
+        borderRadius: BorderRadius.circular(50.w),
         image: const DecorationImage(
-            image: AssetImage("assets/icons/headpic.png"))),
+            image: AssetImage("assets/icons/fumaru.png"))),
     child: Image(
         width: 25.w,
         height: 25.h,
@@ -47,48 +52,48 @@ Widget profileIconAndEditButton() {
 
 //setting sections buttons
 
-var imageInfo = <String, String>{
-  "Settings": "settings.png",
-  "Payment details": "credit-card.png",
-  "Achievement": "award.png",
-  "Love": "heart(1).png",
-  "Reminders": "cube.png",
-};
+// var imageInfo = <String, String>{
+//   "Settings": "settings.png",
+//   "Payment details": "credit-card.png",
+//   "Achievement": "award.png",
+//   "Love": "heart(1).png",
+//   "Reminders": "cube.png",
+// };
 
-Widget buildListView(BuildContext context) {
-  return Column(
-    children: [
-      ...List.generate(
-          imageInfo.length,
-          (index) => GestureDetector(
-                onTap: () =>
-                    Navigator.of(context).pushNamed(AppRoutes.SETTINGS),
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 15.h),
-                  child: Row(children: [
-                    Container(
-                      child: Image.asset(
-                          "assets/icons/${imageInfo.values.elementAt(index)}"),
-                      width: 40.w,
-                      height: 40.h,
-                      padding: EdgeInsets.all(7.0),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.w),
-                          color: AppColors.primaryElement),
-                    ),
-                    SizedBox(
-                      width: 15.w,
-                    ),
-                    Text(
-                      imageInfo.keys.elementAt(index),
-                      style: TextStyle(
-                          color: AppColors.primaryText,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.sp),
-                    ),
-                  ]),
-                ),
-              ))
-    ],
-  );
-}
+// Widget buildListView(BuildContext context) {
+//   return Column(
+//     children: [
+//       ...List.generate(
+//           imageInfo.length,
+//           (index) => GestureDetector(
+//                 onTap: () =>
+//                     Navigator.of(context).pushNamed(AppRoutes.SETTINGS),
+//                 child: Container(
+//                   margin: EdgeInsets.only(bottom: 15.h),
+//                   child: Row(children: [
+//                     Container(
+//                       width: 40.w,
+//                       height: 40.h,
+//                       padding: const EdgeInsets.all(7.0),
+//                       decoration: BoxDecoration(
+//                           borderRadius: BorderRadius.circular(10.w),
+//                           color: AppColors.primaryElement),
+//                       child: Image.asset(
+//                           "assets/icons/${imageInfo.values.elementAt(index)}"),
+//                     ),
+//                     SizedBox(
+//                       width: 15.w,
+//                     ),
+//                     Text(
+//                       imageInfo.keys.elementAt(index),
+//                       style: TextStyle(
+//                           color: AppColors.primaryText,
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 16.sp),
+//                     ),
+//                   ]),
+//                 ),
+//               ))
+//     ],
+//   );
+// }
